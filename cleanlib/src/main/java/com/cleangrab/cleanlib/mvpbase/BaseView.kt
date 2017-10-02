52fed06195009990b96cc9b19e4cab50b041daf3
WANG_QINGYUN <foodsready.market@gmail.com>
2017-10-02 13:59:46 +0800

    added BaseView and BasePresenter

---
+++ cleanlib/src/main/java/com/cleangrab/cleanlib/mvpbase/BaseView.kt
@@ -0,0 +1,20 @@
+package com.cleangrab.cleanlib.mvpbase
+
+/**
+ * Created by qingyun.wang on 2/10/17.
+ *
+ * for MVP:
+ *
+ * The Model holds the business logic of the application. It controls how data can be created,
+ * stored, and modified.
+ *
+ * The View is a passive interface that displays data and routes user actions to the Presenter.
+ *
+ * The Presenter acts as the middle man. It retrieves data from the Model and shows it in the View.
+ * It also processes user actions forwarded to it by the View.
+ *
+ */
+
+interface BaseView<P> {
+    fun setPresenter(presenter: P)
+}
